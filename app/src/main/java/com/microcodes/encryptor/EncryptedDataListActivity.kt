1609a271911a66a0640861af6df0066312b65392
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2021-09-23 17:13:10 +0530

    Added Encrypted data list UI

--- app/src/main/java/com/microcodes/encryptor/EncryptedDataListActivity.kt
+++ app/src/main/java/com/microcodes/encryptor/EncryptedDataListActivity.kt
@@ -1,9 +1,18 @@
 package com.microcodes.encryptor
 
 import android.os.Bundle
-import android.text.Html
+import android.transition.Visibility
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.microcodes.encryptor.recyclerViews.DataListRecyclerView
+import com.microcodes.encryptor.utils.Db
 
 class EncryptedDataListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +22,19 @@ class EncryptedDataListActivity : AppCompatActivity() {
         // Editing status bar
         supportActionBar?.title = "Encrypted Data List"
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
+
+//        findViewById<ProgressBar>(R.id.loadingIndicator).visibility = View.VISIBLE
+
+        val data = Db().getEncryptedData("000")
+        findViewById<RecyclerView>(R.id.dataListRecView).apply {
+            adapter = DataListRecyclerView(data)
+            layoutManager = LinearLayoutManager(this@EncryptedDataListActivity)
+        }
+
+//        findViewById<ProgressBar>(R.id.loadingIndicator).visibility = View.GONE
+
+
+
 
 //        val sharedpreferences = getSharedPreferences("Encryptor_Preferences", Context.MODE_PRIVATE)
 //        val deviceId = sharedpreferences.getString("deviceId", "No device Id")!!
--- app/src/main/java/com/microcodes/encryptor/utils/Db.kt
+++ app/src/main/java/com/microcodes/encryptor/utils/Db.kt
@@ -1,5 +1,6 @@
 package com.microcodes.encryptor.utils
 
+import android.annotation.SuppressLint
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.DocumentReference
@@ -7,6 +8,7 @@ import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.getField
 import com.google.firebase.ktx.Firebase
 import com.microcodes.encryptor.models.User
 import kotlinx.coroutines.Dispatchers
@@ -67,9 +69,58 @@ class Db(private val db: FirebaseFirestore = Firebase.firestore) {
                     .get().addOnSuccessListener {
                         db.collection("users")
                             .document(it!!.documents[0].id)
-                            .update("encryptions", FieldValue.arrayUnion(mapOf("message" to message, "value" to data)))
+                            .update(
+                                "encryptions",
+                                FieldValue.arrayUnion(mapOf("message" to message, "value" to data, "hash" to data.hashCode().toString()))
+                            )
                     }
             )
+
+        }
+        return res.size() > 0
+    }
+
+    fun getEncryptedData(deviceId: String): List<Map<String, String>> {
+        lateinit var res: QuerySnapshot
+        runBlocking(Dispatchers.IO) {
+            res = Tasks.await(
+                db.collection("users")
+                    .whereEqualTo("deviceId", deviceId)
+                    .get()
+            )
+        }
+
+        return if (res.size() > 0) res.documents[0]["encryptions"] as List<Map<String, String>>
+        else emptyList()
+    }
+
+    fun deleteDataItem(deviceId: String, hash: String): Boolean {
+        lateinit var res:
+                QuerySnapshot
+
+        runBlocking(Dispatchers.IO) {
+            res = Tasks.await(
+                db.collection("users")
+                    .whereEqualTo("deviceId", deviceId)
+                    .get().addOnSuccessListener {
+//                        db.collection("users")
+//                            .document(it!!.documents[0].id)
+//                            .update(
+//                                "encryptions",
+//                                FieldValue.arrayUnion(mapOf("message" to message, "value" to data, "hash" to data.hashCode().toString()))
+//                            )
+                        val data = it.documents[0].getField<Any>("encryptions")
+                            println(data)
+
+                    }
+            )
+//            if (res.size() > 0) {
+//                for (item in it.documents) {
+//                    for (data in item["encryptions"] as List<Map<String, String>> ) {
+//                        println(data["message"])
+//                    }
+//                }
+//            }
 
         }
         return res.size() > 0
